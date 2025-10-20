e8f37cd29905ae942bcb9cfa5cff465d608e2dc3
ehsanahdi <ehsanahadi@ut.ac.ir>
2025-10-20 07:57:51 +0330

    basis of this algorithm writen

---
+++ Best-first-search.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+    def __lt__(self, other):
+        return False
